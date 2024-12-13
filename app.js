1510c181b01c7c7e1875167d2a6fb83b63ecbe36
Calnexus <laredkevoh@gmail.com>
2024-12-13 20:04:27 +0300

    feat: Cors enabled

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const http = require("http");
+const cors = require("cors");
 const aiRoutes = require("./routes/AiRoutes");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
